79f8f107a52685f0d2dd2f00f397141b26cc591f
ervinerviansyah <ervin.erviansyah2003@gmail.com>
2026-06-11 17:53:25 +0700

    feat(plugins): add productivity tools, ui enhancement, and custom keymaps
    - Add Wakatime for automatic coding time tracking.
    - Add Neocord with custom configurations for Discord Rich Presence.
    - Add Hlchunk.nvim with configurations for better indent line visual
    guides.
    - Add nvzone/volt as a core dependency for Nvzone ecosystem.
    - Add nvzone/menu to enable custom right-click context menu options.
    - Add Typr plugin for in-editor typing practice and statistics.
    - Add nvzone/floaterm for managing interactive floating terminal
    windows.
    - Add showkeys plugin to display active keystrokes on screen.
    - Add triforce.nvim to track internal editor stats and RPG-like
    progression.
    - Add lazygit.nvim for seamless terminal-based Git interface
    integration.
    - Restructure and implement custom keymaps (including mappings for
    Timerly,
      Floaterm, Triforce, and LazyGit).

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,43 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Keymap Plugin Menu
+-- Keyboard users
+vim.keymap.set("n", "<C-t>", function()
+  require("menu").open "default"
+end, {})
+
+-- mouse users + nvimtree users!
+vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
+  require("menu.utils").delete_old_menus()
+
+  vim.cmd.exec '"normal! \\<RightMouse>"'
+
+  -- clicked buf
+  local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+  local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
+
+  require("menu").open(options, { mouse = true })
+end, {})
+
+-- Keymap Plugin Timerly
+vim.api.nvim_create_user_command("Timer", function()
+  vim.o.showtabline = 0
+  vim.o.laststatus = 0
+  vim.wo.number = false
+  vim.o.scl = "no"
+  vim.o.cmdheight = 0
+  vim.cmd "TimerlyToggle"
+end, {})
+
+vim.keymap.set("n", "<leader>tt", "<cmd>Timer<cr>", { desc = "Start Focus Timer" })
+
+-- Keymap Plugin Floaterm
+vim.keymap.set("n", "<C-p>", "<cmd>FloatermToggle<cr>", { desc = "Toggle Floaterm", silent = true })
+
+-- Keymap Plugin Triforce
+vim.keymap.set("n", "<leader>tp", require("triforce").show_profile, { desc = "Show Triforce Stats" })
+
+-- Keymap Plugin Lazygit
+vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "Toggle LazyGit", silent = true })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
 
@@ -17,12 +17,118 @@ return {
   { import = "nvchad.blink.lazyspec" },
 
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css"
-  		},
-  	},
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
+    },
+  },
+
+  -- Plugin Wakatime
+  {
+    "wakatime/vim-wakatime",
+    lazy = false,
+  },
+
+  -- Plugin Neocord (buat notifikasi di Discord)
+  {
+    "IogaMaster/neocord",
+    event = "VeryLazy",
+    config = function()
+      require "configs.neocord"
+    end,
+  },
+
+  -- Plugin Hlchunk
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require "configs.hlchunk"
+      --- require("hlchunk").setup {}
+    end,
+  },
+
+  -- Dependency Plugin dari Nvzone
+  {
+    "nvzone/volt",
+    lazy = true,
+  },
+
+  -- Plugin Menu (klik kanan)
+  {
+    "nvzone/menu",
+    lazy = true,
+  },
+
+  -- Plugin Timerly (pomodoro)
+  {
+    "nvzone/timerly",
+    dependencies = "nvzone/volt",
+    cmd = "TimerlyToggle",
+    opts = {}, -- optional
+  },
+
+  -- Plugin Minty (color picker)
+  {
+    "nvzone/minty",
+    cmd = { "Shades", "Huefy" },
+  },
+
+  -- Plugin Typr (latihan mengetik)
+  {
+    "nvzone/typr",
+    dependencies = "nvzone/volt",
+    opts = {},
+    cmd = { "Typr", "TyprStats" },
+  },
+
+  -- Plugin Floaterm (munculin terminal)
+  {
+    "nvzone/floaterm",
+    dependencies = "nvzone/volt",
+    opts = {},
+    cmd = "FloatermToggle",
+  },
+
+  -- Plugin show keys
+  {
+    "nvzone/showkeys",
+    cmd = "ShowkeysToggle",
+    opts = {
+      timeout = 1,
+      maxkeys = 5,
+      -- more opts
+    },
+  },
+
+  -- Plugin Triforce
+  {
+    "gisketch/triforce.nvim",
+    dependencies = { "nvzone/volt" },
+    opts = {},
+    config = function()
+      require "configs.triforce"
+    end,
+  },
+
+  -- Plugin Lazygit
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
   },
 }
